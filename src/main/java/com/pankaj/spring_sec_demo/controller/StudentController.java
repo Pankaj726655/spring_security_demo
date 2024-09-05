7ee0102f316c5191ce58b131e1d02e4378bcc014
Pankaj726655 <pp26421@gmail.com>
2024-09-05 22:54:55 +0530

    again push

---
+++ src/main/java/com/pankaj/spring_sec_demo/controller/StudentController.java
@@ -0,0 +1,32 @@
+package com.pankaj.spring_sec_demo.controller;
+
+import com.pankaj.spring_sec_demo.model.Student;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+    List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "kiran", "java"),
+            new Student(2, "neha", "python")
+    ));
+
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        return students;
+    }
+
+    @PostMapping("students")
+    public void addStudent(@RequestBody Student student){
+        students.add(student);
+    }
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+}
---
+++ src/main/java/com/pankaj/spring_sec_demo/service/MyUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.pankaj.spring_sec_demo.service;
+
+import com.pankaj.spring_sec_demo.dao.UserRepo;
+import com.pankaj.spring_sec_demo.model.User;
+import com.pankaj.spring_sec_demo.model.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repo.findByUsername(username);
+        if(user == null){
+            System.out.println("Invalid user !!");
+            throw new UsernameNotFoundException("User 404");
+        }
+        else {
+            System.out.println("User :::"+username);
+        }
+        return new UserDetailsImpl(user);
+    }
+}
